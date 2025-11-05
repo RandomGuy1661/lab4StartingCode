f960fd2d21b60186dc1c9f142cd1bc0730795290
Eric LaP <ericlap330@gmail.com>
2025-11-05 14:18:10 -0700

    expanded descriptions

--- src/exercise1/MyAList.java
+++ src/exercise1/MyAList.java
@@ -12,34 +12,53 @@ public class MyAList<E> implements Iterable<E> {
 
     private List<E> list; // underlying ArrayList
 
-    // Constructor
+    /**
+     * constructor for the array list
+     */
     public MyAList() {
         list = new ArrayList<>();
     }
 
-    // Adds one item to the list
+    /**
+     * adds an item to the front of the list
+     * @param item an element you want to add to the list
+     */
     public void add(E item) {
         list.add(item);
     }
 
-    // Adds all items from another MyAList to this one
+    /**
+     *  Adds all items from another MyAList to this one
+     * @param items another MyAList
+     */
     public void addAll(MyAList<E> items) {
         for (E element : items.list) {
             list.add(element);
         }
     }
 
-    // Gets the element at a specific index
+    /**
+     * gets the value of an item at the index equal to the input
+     * @param index the identifier for the position at which you would like to get the info of
+     * @return the value of the item at the index
+     */
     public E get(int index) {
         return list.get(index);
     }
 
-    // Removes the element at a specific index
+    /**
+     * removes an item at a specified index
+     * @param index the identifier for what element you element you would like to remove an item
+     */
     public void remove(int index) {
         list.remove(index);
     }
 
-    // Replaces the element at a specific index with a new one
+    /**
+     * Replaces the element at a specific index with a new one
+     * @param index the identifier for what item you would like to replace
+     * @param item the value for the new element you would like to add
+     */
     public void set(int index, E item) {
         list.set(index, item);
     }
