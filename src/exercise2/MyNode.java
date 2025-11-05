da16d08afc74b0e7607a7e241c893248f2794e78
Eric LaP <ericlap330@gmail.com>
2025-11-04 19:27:49 -0700

    finished part 2 of lab 4

--- src/exercise2/MyNode.java
+++ src/exercise2/MyNode.java
@@ -6,5 +6,31 @@ public class MyNode<E>
 	private E element;
 	private MyNode<E> next;
 	private MyNode<E> prev;
+	
+	public MyNode(E element ){
+		this.element = element;
+		this.setNext(null);
+		this.setPrev(null);
+	}
+	
+	public E getElement() {
+		return element;
+	}
+
+	public MyNode<E> getPrev() {
+		return prev;
+	}
+
+	public void setPrev(MyNode<E> prev) {
+		this.prev = prev;
+	}
+
+	public MyNode<E> getNext() {
+		return next;
+	}
+
+	public void setNext(MyNode<E> next) {
+		this.next = next;
+	}
 
 }
